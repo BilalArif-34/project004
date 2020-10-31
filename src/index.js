f3b68a6a63b65057bf260f698c58076de76dcb2e
Bilal Arif <36725442+BilalArif-34@users.noreply.github.com>
2020-11-01 01:27:14 +0300

    basic react app

--- src/index.js
+++ src/index.js
@@ -5,21 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <div><App name="Bilal" age={25} /></div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-
-
-/*
-ReactDOM.render(
-  React.createElement('div',null,React.createElement('span',null,"Hello in span create element")),
-  document.getElementById('root')
-);
-*/
-/*
-ReactDOM.render(
-  <div>Hello World</div>,
-  document.getElementById('root')
-);*/
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
