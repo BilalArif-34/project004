370c90f78c7453e571a273e6fefb720bb7d59ef3
Bilal Arif <36725442+BilalArif-34@users.noreply.github.com>
2020-11-01 01:49:41 +0300

    basic react app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Greeting from './Greeting'
 import Parent from './Parent'
 import reportWebVitals from './reportWebVitals';
