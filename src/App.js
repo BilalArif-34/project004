2f57e728a58e091c189d911d2946cd6383cdcdfa
Bilal Arif <36725442+BilalArif-34@users.noreply.github.com>
2020-11-01 01:26:32 +0300

    basic react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App(props) {
-  return 
-
-        <div>
-          Hello from {props.name}. I am {props.age - 5} years old.
-          <br/>
-          <Hello firstName={name} ></Hello>
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
